207d352fa30e581a5831b9adfdd5d211f67f05b6
Alon Ronder <68230416+Alpha110R@users.noreply.github.com>
2021-11-16 07:41:55 +0200

    Advance formation - cleaner code and more understandable

--- script.js
+++ script.js
@@ -1,84 +1,132 @@
 'use strict';
-let secretNumber = Math.floor(Math.random() * 20) + 1;
-let score = 20;
-let highScore = 0;
-let flagWin = false;
 
+const createDefaultParameters = () => {
+    return {
+        secretNumber: Math.floor(Math.random() * 20) + 1,
+        score: 20,
+        highScore: 0,
+        flagWin: false
+    }
+}
 
-document.querySelector('.again').addEventListener('click', function () {
-    document.querySelector('body').style.backgroundColor = '#222';
-    document.querySelector('.number').style.width = '15rem';
-    restartGame();
-});
+const querySelectors = () => {
+    return {
+        body: document.querySelector('body'),
+        againButton: document.querySelector('.again'),
+        guessedNumber: document.querySelector('.number'),
+        messagePlace: document.querySelector('.message'),
+        checkButton: document.querySelector('.check'),
+        score: document.querySelector('.score'),
+        numberInput: document.querySelector('.guess'),
+        highScore: document.querySelector('.highscore')
+    }
+}
 
-document.querySelector('.check').addEventListener('click', function () {
-    const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
+const subscribeForEvents = () => {
+    selectors.againButton.addEventListener('click', () => {
+        selectors.body.style.backgroundColor = '#222';
+        selectors.guessedNumber.style.width = '15rem';
+        restartGame();
+    });
 
-    if (!guess) {
+    selectors.checkButton.addEventListener('click', checkGuessedNumber)
+}
+
+const displayMessage = (message) => {
+    selectors.messagePlace.textContent = message;
+}
+
+const disableGuessedInput = () => {
+    clearGuessNumberBox();
+    selectors.numberInput.disabled = true;
+}
+
+const enabledGuessedInput = () => {
+    clearGuessNumberBox();
+    selectors.numberInput.disabled = false;
+}
+
+const clearGuessNumberBox = () => {
+    selectors.numberInput.value = null;
+}
+
+const restartGame = () => {
+    enabledGuessedInput();
+    defaultParameters.secretNumber = Math.floor(Math.random() * 20) + 1;
+    defaultParameters.score = 20;
+    defaultParameters.flagWin = false;
+    selectors.guessedNumber.textContent = '?';
+    displayMessage('Start guessing...');
+    selectors.score.textContent = 20;
+    selectors.numberInput.value = '';
+}
+
+const winGame = (score, highScore) => {
+    disableGuessedInput();
+    selectors.body.style.backgroundColor = '#60b347';
+    selectors.guessedNumber.style.width = '30rem';
+    selectors.guessedNumber.textContent = defaultParameters.secretNumber;
+    displayMessage('🎉Correct number');
+    defaultParameters.flagWin = true;
+    assignHighScore(score, highScore);
+}
+
+const assignHighScore = (score, highScore) => {
+    if (score <= highScore) {
+        return
+    }
+
+    highScore = score;
+    selectors.highScore.textContent = highScore;
+}
+
+const loseGame = () => {
+    disableGuessedInput();
+    selectors.body.style.backgroundColor = '#DC143C';
+    displayMessage('😢YOU LOST THE GAME');
+    selectors.score.textContent = 0;
+    defaultParameters.flagWin = true;
+}
+
+const hintToGuessTheNumber = (guess, secretNumber, score) => {
+    displayMessage(guess < secretNumber ? 'The guess is too low, go higher!' : 'The guess is to high, go lower!');
+    selectors.score.textContent = score;
+}
+
+const checkGuessedNumber = () => {
+    const guessNumber = Number(selectors.numberInput.value);
+
+    if (!guessNumber) {
         displayMessage('🤷‍♂️ No Number!');
     } else {
-        if (flagWin == false) {
-            if (guess === secretNumber) {
-                winGame(score, highScore);
-            } else {
-                score--;
-                if (score < 1) {
-                    loseGame();
-                }
-                else {
-                    hintToGuessTheNumber(guess, secretNumber, score);
-                }
-            }
-        }
-        else {
-            if (score > 0)
-                displayMessage('🙌👏 YOU WON ALREADY!');
-        }
-    }
-});
-
-const displayMessage = function (message) {
-    document.querySelector('.message').textContent = message;
-}
-
-function restartGame() {
-    secretNumber = Math.floor(Math.random() * 20) + 1;
-    score = 20;
-    flagWin = false;
-    document.querySelector('.number').textContent = '?';
-    displayMessage('Start guessing...');
-    document.querySelector('.score').textContent = 20;
-    document.querySelector('.guess').value = '';
-}
-
-const winGame = function (score, highScore) {
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').textContent = secretNumber;
-    displayMessage('🎉Correct number');
-    flagWin = true;
-    checkHighScore(score, highScore);
-}
-
-const checkHighScore = function (score, highScore) {
-    if (score > highScore) {
-        highScore = score;
-        document.querySelector('.highscore').textContent = highScore;
+        handleGuess(guessNumber)
     }
 }
 
-const loseGame = function () {
-    document.querySelector('body').style.backgroundColor = '#DC143C';
-    displayMessage('😢YOU LOST THE GAME');
-    document.querySelector('.score').textContent = 0;
-    flagWin = true;
+const handleGuess = (guessNumber) => {
+    if (defaultParameters.flagWin === true) {
+        displayMessage('🙌👏 YOU WON ALREADY!');
+        return
+    }
+
+    if (guessNumber === defaultParameters.secretNumber) {
+        winGame(defaultParameters.score, defaultParameters.highScore);
+        return
+    }
+
+    defaultParameters.score--;
+
+    if (defaultParameters.score < 1) {
+        loseGame();
+        return
+    }
+
+    hintToGuessTheNumber(guessNumber, defaultParameters.secretNumber, defaultParameters.score);
 }
 
-const hintToGuessTheNumber = function (guess, secretNumber, score) {
-    displayMessage(guess < secretNumber ? 'The guess is too low, go higher!' : 'The guess is to high, go lower!');
-    document.querySelector('.score').textContent = score;
-}
 
 
+const defaultParameters = createDefaultParameters();
+const selectors = querySelectors();
 
+subscribeForEvents();
